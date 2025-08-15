17dc243b1ad46183b0dfee9aba7ddb45b5a1b63e
Sandeep Shinde <urssandy84@gmail.com>
2025-08-15 14:33:18 +0530

    Upgraded Kotlin version

--- materialized-view/build.gradle.kts
+++ materialized-view/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.25" apply false
-    kotlin("plugin.spring") version "1.9.25" apply false
+    kotlin("jvm") version "2.1.20" apply false
+    kotlin("plugin.spring") version "2.1.20" apply false
     id("org.springframework.boot") version "3.5.4" apply false
     id("io.spring.dependency-management") version "1.1.7" apply false
     id("com.github.davidmc24.gradle.plugin.avro") version "1.9.1" apply false
